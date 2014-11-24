c77337f62783d4bd87ee544fec1864c76f8c0d37
chen1993 <chen1993@163.com>
2014-11-24 15:46:13 +0800

    rountine commit!

--- View/adfmsrc/dcm/template/TemplateBackingBean.java
+++ View/adfmsrc/dcm/template/TemplateBackingBean.java
@@ -4,6 +4,37 @@ import common.ADFUtils;
 
 import javax.faces.event.ActionEvent;
 
-public class TemplateBackingBean {
+import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.view.rich.component.rich.nav.RichCommandButton;
+import oracle.adf.view.rich.component.rich.nav.RichCommandToolbarButton;
 
+import oracle.jbo.RowSetIterator;
+
+public class TemplateBackingBean {
+    private RichCommandToolbarButton ctb;
+    
+    
+    public boolean is_Disabled(){
+        DCIteratorBinding iter=ADFUtils.findIterator("DcmTemplateColumnView1Iterator");
+        RowSetIterator rowiter= iter.getRowSetIterator();
+        if(rowiter.hasNext()){
+            return false;
+        }
+        else  return true;
+    }
+    public void setCtb(RichCommandToolbarButton ctb) {
+        this.ctb = ctb;
+    }
+
+    public RichCommandToolbarButton getCtb() {
+          if (ctb==null)
+            return ctb;
+        DCIteratorBinding iter=ADFUtils.findIterator("DcmTemplateColumnView1Iterator");
+        RowSetIterator rowiter= iter.getRowSetIterator();
+        if(rowiter.hasNext()){
+            ctb.setDisabled(false);
+        }
+        else ctb.setDisabled(true);
+       return ctb;
+    }
 }
