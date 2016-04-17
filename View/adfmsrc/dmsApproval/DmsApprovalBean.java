1394fb5e6545d9d406b2de88db8315a29622eceb
wtgdper <tiegang.wang@hand-china.com>
2016-04-17 23:21:24 +0800

    审批人配置

--- View/adfmsrc/dmsApproval/DmsApprovalBean.java
+++ View/adfmsrc/dmsApproval/DmsApprovalBean.java
@@ -5,10 +5,19 @@ import common.ADFUtils;
 import common.DmsUtils;
 import common.JSFUtils;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import javax.faces.event.ValueChangeEvent;
+
+import javax.faces.model.SelectItem;
 
 import oracle.adf.share.ADFContext;
 import oracle.adf.view.rich.component.rich.RichPopup;
@@ -17,10 +26,12 @@ import oracle.adf.view.rich.component.rich.output.RichOutputText;
 
 import oracle.jbo.Row;
 import oracle.jbo.ViewObject;
+import oracle.jbo.server.DBTransaction;
 import oracle.jbo.server.ViewRowImpl;
 
 public class DmsApprovalBean {
     private RichPopup approvalPop;
+    private List<SelectItem> entityList;
 
     public DmsApprovalBean() {
         super();
@@ -39,10 +50,47 @@ public class DmsApprovalBean {
         }
         
         Row row = vo.getCurrentRow();
-        String valuesId = row.getAttribute("ValueSetId").toString();
+        String comId = row.getAttribute("ValueSetId").toString();
+        
+        this.entityList = this.getEntityList(comId);
         
         RichPopup.PopupHints hints = new RichPopup.PopupHints();
         this.approvalPop.show(hints);
+    }
+    
+    public List<SelectItem> getEntityList(String comId){
+        List<SelectItem> list = new ArrayList<SelectItem>();
+        DBTransaction trans = (DBTransaction)DmsUtils.getDmsApplicationModule().getTransaction();
+        Statement stat = trans.createStatement(1);
+        String sql = "SELECT V.ID,V.SOURCE FROM DCM_COM_VS T,DMS_VALUE_SET V WHERE T.IS_APPROVAL = 'Y' "
+            + "AND T.VALUE_SET_ID = V.ID AND V.LOCALE = 'zh_CN' AND T.COMBINATION_ID = '" + comId + "'";
+        ResultSet rs;
+        String source = "";
+        try {
+            rs = stat.executeQuery(sql);
+            if(rs.next()){
+                source = rs.getString("SOURCE");    
+            }else{
+                rs.close();
+                stat.close();
+                return list;
+            }
+            rs.close();
+            
+            String sql1 = "SELECT T.CODE,T.MEANING FROM " + source + " T WHERE T.LOCALE = '" 
+                          + ADFContext.getCurrent().getLocale() + "'";
+            ResultSet eRs = stat.executeQuery(sql1);    
+            while(eRs.next()){
+                SelectItem sim = new SelectItem();
+                sim.setValue(eRs.getString("CODE"));
+                sim.setLabel(eRs.getString("MEANING"));
+                list.add(sim);
+            }
+            eRs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void setApprovalPop(RichPopup approvalPop) {
@@ -53,4 +101,11 @@ public class DmsApprovalBean {
         return approvalPop;
     }
 
+    public void setEntityList(List<SelectItem> entityList) {
+        this.entityList = entityList;
+    }
+
+    public List<SelectItem> getEntityList() {
+        return entityList;
+    }
 }
