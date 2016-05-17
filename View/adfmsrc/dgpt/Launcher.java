ff83aff531c57137c1465debe79a116102046495
wtgdper <tiegang.wang@hand-china.com>
2016-05-17 13:55:12 +0800

    新增Tab页

---
+++ View/adfmsrc/dgpt/Launcher.java
@@ -0,0 +1,17 @@
+package dgpt;
+
+import dms.dynamicShell.TabLauncher;
+
+import javax.faces.event.ActionEvent;
+
+public class Launcher extends TabLauncher{
+    public Launcher() {
+        super();
+    }
+
+    public void launcherBh(ActionEvent actionEvent) {
+        _launchActivity("预计新签项目预算编制表-北和",
+                        "/WEB-INF/dgpt/dms_xqys_bh_tsk.xml#dms_xqys_bh_tsk",
+                        false);
+    }
+}
