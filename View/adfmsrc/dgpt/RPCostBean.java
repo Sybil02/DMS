7f6e7ac04429e8270e224d8fd346a40d4877f536
Sybil02 <chunmei.chen@hand-china.com>
2016-06-12 15:40:51 +0800

    滚动页面

---
+++ View/adfmsrc/dgpt/RPCostBean.java
@@ -0,0 +1,245 @@
+package dgpt;
+
+import common.DmsUtils;
+
+import dcm.PcDataTableModel;
+
+import dms.login.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Map;
+
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+
+import oracle.adf.share.ADFContext;
+
+import oracle.jbo.server.DBTransaction;
+
+public class RPCostBean {
+    private Person curUser;
+    public RPCostBean() {
+        super();
+        this.curUser = (Person)(ADFContext.getCurrent().getSessionScope().get("cur_user"));
+//        this.dataModel = new PcDataTableModel();
+//        List<Map> d = new ArrayList<Map>();
+//        this.dataModel.setWrappedData(d);
+        this.initList();
+    }
+    
+    private String year;
+    private List<SelectItem> yearList;
+    private String entity;
+    private String pLine;
+    private String pname;
+    private List<SelectItem> pnameList;
+    private String version;
+    private List<SelectItem> versionList;
+    private String proType;
+    private String hLine;
+    private String yLine;
+    private String pStart;
+    private String pEnd;
+    private String connectId;
+    public static final String TYPE_ZZX="ZX";
+    
+    
+    private void initList(){
+        this.yearList = queryYears("HLS_YEAR_C");
+        this.pnameList = queryValues("PRO_PLAN_COST_HEADER","PROJECT_NAME");
+        this.versionList = queryValues("PRO_PLAN_COST_HEADER","VERSION");
+    }
+
+    //年份下拉列表
+    private List<SelectItem> queryYears(String source){
+        DBTransaction trans = (DBTransaction)DmsUtils.getDmsApplicationModule().getTransaction();
+        Statement stat = trans.createStatement(DBTransaction.DEFAULT);
+        String sql = "SELECT CODE,MEANING FROM "+ source;
+        List<SelectItem> values = new ArrayList<SelectItem>();
+        ResultSet rs;
+
+        try {
+            rs = stat.executeQuery(sql);
+            while(rs.next()){
+                SelectItem sim = new SelectItem(rs.getString("CODE"),rs.getString("MEANING"));
+                values.add(sim);
+            }
+            rs.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return values;
+    }
+    
+    //其他下拉列表
+    private List<SelectItem> queryValues(String source,String col){
+        DBTransaction trans = (DBTransaction)DmsUtils.getDmsApplicationModule().getTransaction();
+        Statement stat = trans.createStatement(DBTransaction.DEFAULT);
+        String sql = "SELECT DISTINCT "+col+" FROM "+source;
+        List<SelectItem> values = new ArrayList<SelectItem>();
+        ResultSet rs;
+        try {
+            rs = stat.executeQuery(sql);
+            while(rs.next()){
+                SelectItem sim = new SelectItem(rs.getString(col),rs.getString(col));
+                values.add(sim);
+            }
+            rs.close();
+            stat.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return values;
+    }  
+    
+    //项目名称下拉框change
+    public void projectChange(ValueChangeEvent valueChangeEvent) {
+        pname =(String) valueChangeEvent.getNewValue();
+            if(year==null||version==null||pname==null){
+                return;
+            }else{
+//                this.queryData();
+//                this.createTableModel();
+            }
+    }
+    
+    //版本下拉框change
+    public void versionChange(ValueChangeEvent valueChangeEvent) {
+        version =(String) valueChangeEvent.getNewValue();
+        if(year==null||version==null||pname==null){
+            return;
+        }else{
+//            this.queryData();
+//            this.createTableModel();
+        }
+    }
+    
+    //年下拉框change
+    public void yearChange(ValueChangeEvent valueChangeEvent) {
+        year = (String)valueChangeEvent.getNewValue();
+        if(year==null||version==null||pname==null){
+            return;
+        }else{
+//            this.queryData();
+//            this.createTableModel();
+        }
+    }
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYearList(List<SelectItem> yearList) {
+        this.yearList = yearList;
+    }
+
+    public List<SelectItem> getYearList() {
+        return yearList;
+    }
+
+    public void setEntity(String entity) {
+        this.entity = entity;
+    }
+
+    public String getEntity() {
+        return entity;
+    }
+
+    public void setPLine(String pLine) {
+        this.pLine = pLine;
+    }
+
+    public String getPLine() {
+        return pLine;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPnameList(List<SelectItem> pnameList) {
+        this.pnameList = pnameList;
+    }
+
+    public List<SelectItem> getPnameList() {
+        return pnameList;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersionList(List<SelectItem> versionList) {
+        this.versionList = versionList;
+    }
+
+    public List<SelectItem> getVersionList() {
+        return versionList;
+    }
+
+    public void setProType(String proType) {
+        this.proType = proType;
+    }
+
+    public String getProType() {
+        return proType;
+    }
+
+    public void setHLine(String hLine) {
+        this.hLine = hLine;
+    }
+
+    public String getHLine() {
+        return hLine;
+    }
+
+    public void setYLine(String yLine) {
+        this.yLine = yLine;
+    }
+
+    public String getYLine() {
+        return yLine;
+    }
+
+    public void setPStart(String pStart) {
+        this.pStart = pStart;
+    }
+
+    public String getPStart() {
+        return pStart;
+    }
+
+    public void setPEnd(String pEnd) {
+        this.pEnd = pEnd;
+    }
+
+    public String getPEnd() {
+        return pEnd;
+    }
+
+    public void setConnectId(String connectId) {
+        this.connectId = connectId;
+    }
+
+    public String getConnectId() {
+        return connectId;
+    }
+}
