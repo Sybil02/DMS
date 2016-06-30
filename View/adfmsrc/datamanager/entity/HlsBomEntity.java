fd82607d5405de19220123dc6485b8aa7bcdcadb
wtgdper <tiegang.wang@hand-china.com>
2016-06-30 14:49:01 +0800

    WebService物料

---
+++ View/adfmsrc/datamanager/entity/HlsBomEntity.java
@@ -0,0 +1,281 @@
+package datamanager.entity;
+
+import java.io.Serializable;
+
+public class HlsBomEntity implements Serializable{
+    private static final long serialVersionUID = 0L;
+    public HlsBomEntity() {
+        super();
+    }
+    
+    private String  matnr  ;
+    private String  maktx  ;
+    private String  werks  ;
+    private String  mtart  ;
+    private String  meins  ;
+    private String  mtbez  ;
+    private String  dispo  ;
+    private String  zgood  ;
+    private String  stprs  ;
+    private String  peinh  ;
+    private String  zplp2  ;
+    private String  lvorm  ;
+    private String  note1  ;
+    private String  note2  ;
+    private String  note3  ;
+    private String  note4  ;
+    private String  note5  ;
+    private String  note6  ;
+    private String  note7  ;
+    private String  note8  ;
+    private String  note9  ;
+    private String  note10  ;
+    private String  note11  ;
+    private String  note12  ;
+    private String  note13  ;
+    private String  note14  ;
+    private String  note15  ;
+    private String  ifflg  ;
+    private String  ifmsg  ;
+    private String  msgid  ;
+
+    public void setMatnr(String matnr) {
+        this.matnr = matnr;
+    }
+
+    public String getMatnr() {
+        return matnr;
+    }
+
+    public void setMaktx(String maktx) {
+        this.maktx = maktx;
+    }
+
+    public String getMaktx() {
+        return maktx;
+    }
+
+    public void setWerks(String werks) {
+        this.werks = werks;
+    }
+
+    public String getWerks() {
+        return werks;
+    }
+
+    public void setMtart(String mtart) {
+        this.mtart = mtart;
+    }
+
+    public String getMtart() {
+        return mtart;
+    }
+
+    public void setMeins(String meins) {
+        this.meins = meins;
+    }
+
+    public String getMeins() {
+        return meins;
+    }
+
+    public void setMtbez(String mtbez) {
+        this.mtbez = mtbez;
+    }
+
+    public String getMtbez() {
+        return mtbez;
+    }
+
+    public void setDispo(String dispo) {
+        this.dispo = dispo;
+    }
+
+    public String getDispo() {
+        return dispo;
+    }
+
+    public void setZgood(String zgood) {
+        this.zgood = zgood;
+    }
+
+    public String getZgood() {
+        return zgood;
+    }
+
+    public void setStprs(String stprs) {
+        this.stprs = stprs;
+    }
+
+    public String getStprs() {
+        return stprs;
+    }
+
+    public void setPeinh(String peinh) {
+        this.peinh = peinh;
+    }
+
+    public String getPeinh() {
+        return peinh;
+    }
+
+    public void setZplp2(String zplp2) {
+        this.zplp2 = zplp2;
+    }
+
+    public String getZplp2() {
+        return zplp2;
+    }
+
+    public void setLvorm(String lvorm) {
+        this.lvorm = lvorm;
+    }
+
+    public String getLvorm() {
+        return lvorm;
+    }
+
+    public void setNote1(String note1) {
+        this.note1 = note1;
+    }
+
+    public String getNote1() {
+        return note1;
+    }
+
+    public void setNote2(String note2) {
+        this.note2 = note2;
+    }
+
+    public String getNote2() {
+        return note2;
+    }
+
+    public void setNote3(String note3) {
+        this.note3 = note3;
+    }
+
+    public String getNote3() {
+        return note3;
+    }
+
+    public void setNote4(String note4) {
+        this.note4 = note4;
+    }
+
+    public String getNote4() {
+        return note4;
+    }
+
+    public void setNote5(String note5) {
+        this.note5 = note5;
+    }
+
+    public String getNote5() {
+        return note5;
+    }
+
+    public void setNote6(String note6) {
+        this.note6 = note6;
+    }
+
+    public String getNote6() {
+        return note6;
+    }
+
+    public void setNote7(String note7) {
+        this.note7 = note7;
+    }
+
+    public String getNote7() {
+        return note7;
+    }
+
+    public void setNote8(String note8) {
+        this.note8 = note8;
+    }
+
+    public String getNote8() {
+        return note8;
+    }
+
+    public void setNote9(String note9) {
+        this.note9 = note9;
+    }
+
+    public String getNote9() {
+        return note9;
+    }
+
+    public void setNote10(String note10) {
+        this.note10 = note10;
+    }
+
+    public String getNote10() {
+        return note10;
+    }
+
+    public void setNote11(String note11) {
+        this.note11 = note11;
+    }
+
+    public String getNote11() {
+        return note11;
+    }
+
+    public void setNote12(String note12) {
+        this.note12 = note12;
+    }
+
+    public String getNote12() {
+        return note12;
+    }
+
+    public void setNote13(String note13) {
+        this.note13 = note13;
+    }
+
+    public String getNote13() {
+        return note13;
+    }
+
+    public void setNote14(String note14) {
+        this.note14 = note14;
+    }
+
+    public String getNote14() {
+        return note14;
+    }
+
+    public void setNote15(String note15) {
+        this.note15 = note15;
+    }
+
+    public String getNote15() {
+        return note15;
+    }
+
+    public void setIfflg(String ifflg) {
+        this.ifflg = ifflg;
+    }
+
+    public String getIfflg() {
+        return ifflg;
+    }
+
+    public void setIfmsg(String ifmsg) {
+        this.ifmsg = ifmsg;
+    }
+
+    public String getIfmsg() {
+        return ifmsg;
+    }
+
+    public void setMsgid(String msgid) {
+        this.msgid = msgid;
+    }
+
+    public String getMsgid() {
+        return msgid;
+    }
+}
--- View/adfmsrc/datamanager/ws/DataManager.java
+++ View/adfmsrc/datamanager/ws/DataManager.java
@@ -1,5 +1,7 @@
 package datamanager.ws;
 
+import datamanager.entity.HlsBomEntity;
+
 import dms.quartz.utils.DBConnUtils;
 
 import java.sql.Connection;
@@ -14,57 +16,57 @@ public class DataManager {
         super();
     }
     
-    public String getMsgId(String msgId){
-        return msgId;    
-    }
-    
-    public SapBomEntity insertSapBom(SapBomEntity sbe,String msgId){
+    public HlsBomEntity SyncWuliao(HlsBomEntity bom){
         Connection conn = DBConnUtils.getJNDIConnection("jdbc/DMSConnDS");
-        String sql = "INSERT INTO HPDW.SAP_BOM_MATERIAL_DATA (CUSTOM, MCODE, MDESC, FACTORY, MTYPE, MEINS, MTYPEDESC, MRPCONTROL, MCATORY, STPRICE, PRICEUNIT, PLANPRICE, D_FLAG, NOTE1, NOTE2, " +
-            "NOTE3, NOTE4, NOTE5, NOTE6, NOTE7, NOTE8, NOTE9, NOTE10, NOTE11, NOTE12, NOTE13, NOTE14, NOTE15, IFFLG, IFMSG)" + 
+        String sql = "INSERT INTO DMS_HLS_BOM (MATNR, MAKTX, WERKS, MTART, MEINS, MTBEZ, DISPO, ZGOOD, STPRS, PEINH, ZPLP2, " +
+            "LVORM, NOTE1, NOTE2, NOTE3, NOTE4, NOTE5, NOTE6, NOTE7, NOTE8, NOTE9, NOTE10, NOTE11, NOTE12, NOTE13, NOTE14, NOTE15, IFFLG, IFMSG, MSGID) " + 
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         PreparedStatement stat = null;
         try {
             stat = conn.prepareStatement(sql);
-            stat.setString(1, sbe.getCustom());
-            stat.setString(2, sbe.getMcode());
-            stat.setString(3, sbe.getMdesc());
-            stat.setString(4, sbe.getFactory());
-            stat.setString(5, sbe.getMtype());
-            stat.setString(6, sbe.getMeins());
-            stat.setString(7, sbe.getMtypedesc());
-            stat.setString(8, sbe.getMrpcontrol());
-            stat.setString(9, sbe.getMcatory());
-            stat.setString(10, sbe.getStprice());
-            stat.setString(11, sbe.getPriceunit());
-            stat.setString(12, sbe.getPlanprice());
-            stat.setString(13, sbe.getD_flag());
-            stat.setString(14, sbe.getNote1());
-            stat.setString(15, sbe.getNote2());
-            stat.setString(16, sbe.getNote3());
-            stat.setString(17, sbe.getNote4());
-            stat.setString(18, sbe.getNote5());
-            stat.setString(19, sbe.getNote6());
-            stat.setString(20, sbe.getNote7());
-            stat.setString(21, sbe.getNote8());
-            stat.setString(22, sbe.getNote9());
-            stat.setString(23, sbe.getNote10());
-            stat.setString(24, sbe.getNote11());
-            stat.setString(25, sbe.getNote12());
-            stat.setString(26, sbe.getNote13());
-            stat.setString(27, sbe.getNote14());
-            stat.setString(28, sbe.getNote15());
-            stat.setString(29, sbe.getIfflg());
-            stat.setString(30, sbe.getIfmsg());
+            stat.setString(1, bom.getMatnr());
+            stat.setString(2, bom.getMaktx());
+            stat.setString(3, bom.getWerks());
+            stat.setString(4, bom.getMtart());
+            stat.setString(5, bom.getMeins());
+            stat.setString(6, bom.getMtbez());
+            stat.setString(7, bom.getDispo());
+            stat.setString(8, bom.getZgood());
+            stat.setString(9, bom.getStprs());
+            stat.setString(10, bom.getPeinh());
+            stat.setString(11, bom.getZplp2());
+            stat.setString(12, bom.getLvorm());
+            stat.setString(13, bom.getNote1());
+            stat.setString(14, bom.getNote2());
+            stat.setString(15, bom.getNote3());
+            stat.setString(16, bom.getNote4());
+            stat.setString(17, bom.getNote5());
+            stat.setString(18, bom.getNote6());
+            stat.setString(19, bom.getNote7());
+            stat.setString(20, bom.getNote8());
+            stat.setString(21, bom.getNote9());
+            stat.setString(22, bom.getNote10());
+            stat.setString(23, bom.getNote11());
+            stat.setString(24, bom.getNote12());
+            stat.setString(25, bom.getNote13());
+            stat.setString(26, bom.getNote14());
+            stat.setString(27, bom.getNote15());
+            stat.setString(28, bom.getIfflg());
+            stat.setString(29, bom.getIfmsg());
+            stat.setString(30, bom.getMsgid());
             stat.executeUpdate();
             conn.commit();
             stat.close();
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
-            sbe.setIfflg("E");
-            sbe.setIfmsg(e.getMessage());
-            return sbe;
+            bom.setIfflg("E");
+            if(e.getMessage().length() > 50){
+                bom.setIfmsg(e.getMessage().substring(0, 49));
+            }else{
+                bom.setIfmsg(e.getMessage());
+            }
+            return bom;
         }finally{
             try{
                 if(stat != null){
@@ -76,8 +78,8 @@ public class DataManager {
                 e.printStackTrace();    
             }
         }
-        sbe.setIfflg("S");
-        return sbe;    
+        bom.setIfflg("S");
+        return bom;    
     }
     
 }
