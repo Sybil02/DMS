69f789b98d8c2b8aeb81caeb83b522528f280523
Sybil02 <chunmei.chen@hand-china.com>
2016-09-23 18:50:33 +0800

    infa权限控制

--- View/adfmsrc/infa/InfaAuthrityBean.java
+++ View/adfmsrc/infa/InfaAuthrityBean.java
@@ -2,12 +2,28 @@ package infa;
 
 import common.ADFUtils;
 
+import common.DmsUtils;
+
+import common.lov.DmsComBoxLov;
+
+import common.lov.ValueSetRow;
+
+import dms.login.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import java.util.List;
 
 import javax.faces.event.ActionEvent;
 
+import javax.faces.model.SelectItem;
+
+import oracle.adf.share.ADFContext;
 import oracle.adf.view.rich.component.rich.RichPopup;
 import oracle.adf.view.rich.component.rich.data.RichTable;
 
@@ -17,14 +33,55 @@ import oracle.jbo.Key;
 import oracle.jbo.Row;
 import oracle.jbo.RowSetIterator;
 import oracle.jbo.ViewObject;
+import oracle.jbo.server.DBTransaction;
 
 public class InfaAuthrityBean {
     private RichTable authedTable;
     private RichPopup workflowPop;
     private RichTable unAuthedTable;
+    private String roleName;
+    private Person person =
+            (Person)ADFContext.getCurrent().getSessionScope().get("cur_user");
+    private DmsComBoxLov roleLov;
 
     public InfaAuthrityBean() {
         super();
+        this.initRoleLov();
+    }
+    
+    public void initRoleLov(){
+        List<SelectItem> value = new ArrayList<SelectItem>();
+        DBTransaction trans = (DBTransaction)DmsUtils.getDmsApplicationModule().getTransaction();
+        Statement stat = trans.createStatement(DBTransaction.DEFAULT);
+        String sql = "SELECT T.ID,T.ROLE_NAME FROM DMS_ROLE T WHERE T.LOCALE='"+this.person.getLocale()+
+                    "'  AND T.ENABLE_FLAG = 'Y' ORDER BY T.ROLE_NAME";
+        System.out.println(sql);
+        ResultSet rs ;
+        try {
+            rs = stat.executeQuery(sql);
+            List<ValueSetRow> list = new ArrayList<ValueSetRow>();
+            while(rs.next()){
+                SelectItem item = new SelectItem();
+                item.setLabel(rs.getString("ID"));
+                item.setValue(rs.getString("ROLE_NAME"));
+                value.add(item);
+                ValueSetRow vsr = new ValueSetRow(rs.getString("ID"),rs.getString("ROLE_NAME"),rs.getString("ID"));
+                list.add(vsr);
+                this.roleName = rs.getString("ROLE_NAME");
+            }
+            this.roleLov = new DmsComBoxLov(list);
+            ViewObject vo = ADFUtils.findIterator("DmsEnabledRoleIterator").getViewObject();
+            String wc = " ROLE_NAME = '" + this.roleName + "'";
+            vo.setWhereClause(wc);
+            vo.executeQuery();
+            if (vo.hasNext()) {
+                Row row = vo.first();
+                vo.setCurrentRow(row);
+            }
+//            AdfFacesContext.getCurrentInstance().addPartialTarget(this.authedTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void remove(ActionEvent actionEvent) {
@@ -98,5 +155,21 @@ public class InfaAuthrityBean {
 
     public RichTable getUnAuthedTable() {
         return unAuthedTable;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleLov(DmsComBoxLov roleLov) {
+        this.roleLov = roleLov;
+    }
+
+    public DmsComBoxLov getRoleLov() {
+        return roleLov;
     }
 }
--- View/adfmsrc/odi11g/scene/SceneAuthorityBean.java
+++ View/adfmsrc/odi11g/scene/SceneAuthorityBean.java
@@ -2,11 +2,27 @@ package odi11g.scene;
 
 import common.ADFUtils;
 
+import common.DmsUtils;
+
+import common.lov.DmsComBoxLov;
+
+import common.lov.ValueSetRow;
+
+import dms.login.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.event.ActionEvent;
 
+import javax.faces.model.SelectItem;
+
+import oracle.adf.share.ADFContext;
 import oracle.adf.view.rich.component.rich.RichPopup;
 import oracle.adf.view.rich.component.rich.data.RichTable;
 import oracle.adf.view.rich.component.rich.nav.RichCommandButton;
@@ -17,15 +33,55 @@ import oracle.jbo.Key;
 import oracle.jbo.Row;
 import oracle.jbo.RowSetIterator;
 import oracle.jbo.ViewObject;
+import oracle.jbo.server.DBTransaction;
 
 public class SceneAuthorityBean {
     private RichPopup popup;
     private RichTable unassignedScene;
     private RichTable assignedScene;
+    private String roleName;
+    private Person person =
+            (Person)ADFContext.getCurrent().getSessionScope().get("cur_user");
+    private DmsComBoxLov roleLov;
 
     public SceneAuthorityBean() {
+        this.initinitRoleLov();
     }
 
+    public void initinitRoleLov(){
+        List<SelectItem> value = new ArrayList<SelectItem>();
+        DBTransaction trans = (DBTransaction)DmsUtils.getDmsApplicationModule().getTransaction();
+        Statement stat = trans.createStatement(DBTransaction.DEFAULT);
+        String sql = "SELECT T.ID,T.ROLE_NAME FROM DMS_ROLE T WHERE T.LOCALE='"+this.person.getLocale()+
+                    "'  AND T.ENABLE_FLAG = 'Y' ORDER BY T.ROLE_NAME";
+        System.out.println(sql);
+        ResultSet rs ;
+        try {
+            rs = stat.executeQuery(sql);
+            List<ValueSetRow> list = new ArrayList<ValueSetRow>();
+            while(rs.next()){
+                SelectItem item = new SelectItem();
+                item.setLabel(rs.getString("ID"));
+                item.setValue(rs.getString("ROLE_NAME"));
+                value.add(item);
+                ValueSetRow vsr = new ValueSetRow(rs.getString("ID"),rs.getString("ROLE_NAME"),rs.getString("ID"));
+                list.add(vsr);
+                this.roleName = rs.getString("ROLE_NAME");
+            }
+            this.roleLov = new DmsComBoxLov(list);
+            ViewObject vo = ADFUtils.findIterator("DmsEnabledRoleIterator").getViewObject();
+            String wc = " ROLE_NAME = '" + this.roleName + "'";
+            vo.setWhereClause(wc);
+            vo.executeQuery();
+            if (vo.hasNext()) {
+                Row row = vo.first();
+                vo.setCurrentRow(row);
+            }
+        //            AdfFacesContext.getCurrentInstance().addPartialTarget(this.authedTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     public void setPopup(RichPopup popup) {
         this.popup = popup;
     }
@@ -100,5 +156,21 @@ public class SceneAuthorityBean {
 
     public RichTable getAssignedScene() {
         return assignedScene;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleLov(DmsComBoxLov roleLov) {
+        this.roleLov = roleLov;
+    }
+
+    public DmsComBoxLov getRoleLov() {
+        return roleLov;
     }
 }
